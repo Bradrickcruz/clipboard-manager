8af98d7d0cd485fe0375b578aa38acdc998bc45a
Bryan H Cruz <candorrick@gmail.com>
2023-06-07 19:58:08 -0300

    remove botões de colar e cria um setInterval que verifica o clipboard para salvar no state a cada 500ms

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const { clipboard } = window.require('electron');
 
 function App() {
   const [clipboardItems, setClipboardItems] = useState([]);
+  const [clipboardLastItem, setClipboardLastItem] = useState("");
+
+  setInterval(() => {
+    const text = clipboard.readText();
+    if (text !== clipboardLastItem) {
+      console.log('Texto copiado:', text);
+      setClipboardLastItem(text);
+    }
+  }, 500);
+
+  useEffect(()=>{
+    setClipboardItems((prevItems) => [...prevItems, clipboardLastItem]);
+  },[clipboardLastItem])
 
   const handleClearClipboard = () => {
     setClipboardItems([]);
   };
 
-  const handlePaste = () => {
-    const text = clipboard.readText();
-    setClipboardItems((prevItems) => [...prevItems, text]);
-  };
-
-  const handlePasteImage = () => {
-    const image = clipboard.readImage();
-    // setClipboardItems((prevItems) => [...prevItems, image]);
-    // Faça algo com a imagem aqui, como salvá-la em algum lugar
-  };
-
   return (
     <div>
       <h1>Clipboard Manager</h1>
-      <button onClick={handlePaste}>Colar Texto</button>
-      <button onClick={handlePasteImage}>Colar Imagem</button>
+      <button onClick={handleClearClipboard}>Limpar Histórico</button>
       <ul>
         {clipboardItems.map((item, index) => (
           <li key={index}>{item}</li>
         ))}
       </ul>
-      <button onClick={handleClearClipboard}>Limpar Histórico</button>
     </div>
   );
 }
